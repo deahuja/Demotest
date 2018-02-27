6216f20105a54b3ff33c94d4c5939dea475713cc
unknown <deepakahuja79@gmail.com>
2018-02-27 22:11:28 +1100

    second commit

---
+++ src/Basics/TotestGit.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class TotestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("To test GitHub");
+
+	}
+
+}
